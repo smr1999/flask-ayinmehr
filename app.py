c8e1437554e1d05e50991bff71a17f4dfe00e32e
smr <mohammad>
2022-08-14 20:37:58 +0430

    Part 4

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
+
+# @app.route('/')
+# def index():
+#     return """
+#     <!Doctype html>
+#     <html>
+#         <head>
+#             <title>
+#             My Flask App 
+#             </title>
+#         </head>
+#         <body>
+#             <h1>Hello World!</h1>
+#         </body>
+#     </html>
+#     """
 
 @app.route('/')
 def index():
-    return '<h1>Hello world</h1>'
-
-@app.route('/about')
-def about():
-    return 'this is about page'
-
-# @app.route('/hello/<name>')
-# @app.route('/hello/<string:name>')
-# @app.route('/hello/<int:name>')
-# @app.route('/hello/<float:name>')
-# @app.route('/hello/<path:name>')
-@app.route('/hello/<name>')
-def hello_name(name):
-    return f'Hello {name}'
+    return render_template('index.html')
