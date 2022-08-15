b66b15edf3895411f3ae31e8143f618540d7b2c6
smr <mohammad>
2022-08-15 11:09:10 +0430

    Part 10

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
-from flask import Flask,render_template,request
-from forms import LoginForm
+from flask import Flask, g,request
+import sqlite3
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+
+
+def create_db():
+    return sqlite3.connect('./db.sqlite3')
+
+
+@app.before_request
+def before_request():
+    g.db = create_db()
+    g.cur = g.db.cursor()
+    print("before")
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    print("after")
+    return response
+
 
 @app.route('/')
 def index():
-    login_form = LoginForm()
-    return render_template('index.html',form=login_form)
-
-
-@app.route('/login',methods=['POST'])
-def login():
-    login_form = LoginForm(request.form)
-    if not login_form.validate_on_submit():
-        print(login_form.errors)
-        return "Error"
-    return "OK"
+    action = request.args.get('action')
+    username = request.args.get('username')
+    password = request.args.get('password')
+    if action and username and password :
+        if action.lower() == 'register':
+            try:
+                g.cur.execute('INSERT INTO users(username,password) values (?,?)',(
+                username,password
+            )) # Do not use fstring because for example fstring can not convert None(python) to null(SQL)
+                g.db.commit()
+                return 'User added'
+            except sqlite3.IntegrityError:
+                g.db.rollback()
+                return 'username dupplicated'
+        elif action.lower() == 'login':
+            g.cur.execute('SELECT * FROM users WHERE username = ? AND password = ?',(username,password))
+            user = g.cur.fetchone()
+            if user:
+                return f'Welcome User {user[1]}'
+            else:
+                return 'User not found'      
+    print("index view")
+    return 'Hello world'
